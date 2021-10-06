68b6727b21a83569ef915555797535ffb0730623
runnerlizard <smolev_sergey@mail.ru>
2021-10-06 23:31:59 +0300

    need to fix signal sending

--- client.c
+++ client.c
@@ -1,12 +1,13 @@
 #include "ft_putnbr_fd.c"
 #include "ft_putstr_fd.c"
+#include <time.h>
 
-int	ft_transform(int serverPid, long long int c)
+int	ft_transform(int serverPid, int c)
 {
 	int		i;
 	int		j;
 	int		k;
-	char	*utf[32];
+	char	utf[32];
 
 	i = 0;
 	while (c > 0)
@@ -15,21 +16,26 @@ int	ft_transform(int serverPid, long long int c)
 		c = c / 2;
 	}
 	j = i;
-	k = 5;
+	k = 4;
 	while (k-- > -1)
 	{
 		utf[i++] = j % 2 + 48;
 		j = j / 2;
 	}
-	while (i >= 0)
-		if (utf[i--] == 48)
+	utf[i] = 0;
+	while (i-- > 0)
+	{
+		ft_putstr_fd("go\n", 1);
+		usleep(1000);
+		if (utf[i] == '0')
 			kill(serverPid, SIGUSR1);
 		else
 			kill(serverPid, SIGUSR2);
-	return (0);
+	}
+	return (serverPid);
 }
 
-int	main	(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int	serverPid;
 	int	i;
@@ -37,9 +43,8 @@ int	main	(int argc, char **argv)
 	if (argc != 3)
 		ft_putstr_fd("Error: 2 args required", 1);
 	serverPid = ft_atoi(argv[1]);
-	ft_putnbr_fd(argv[2][0], 1);
 	i = 0;
-	while (argv[2][i])
-		ft_transform(srverPid, argv[2][i++]);
+	while (argv[2][i++])
+		ft_transform(serverPid, argv[2][i - 1]);
 	return(0);
 }
--- server.c
+++ server.c
@@ -1,32 +1,40 @@
 #include "ft_putnbr_fd.c"
+#include "ft_putstr_fd.c"
+#include <string.h>
 
-void ft_putchar(int i)
+void	ft_hdl(int sig, siginfo_t *info, void *context)
 {
-	static int	i;
-	static int	n;
+	int	pid;
 
-	i = 2;
-	n = 0;
-
-}
-
-void	ft_handler_sigusr1(int sig)
-{
-
-	ft_putnbr_fd(1, 1);
-}
-
-void	ft_handler_sigusr2(int sig)
-{
-	ft_putnbr_fd(1, 1);
+	pid = 0;
+	(void)context;
+	if (info->si_pid)
+		pid = info->si_pid;
+	if (sig == SIGUSR1)
+		ft_putnbr_fd(0 ,1);
+	else if (sig == SIGUSR2)
+		ft_putnbr_fd(1, 1);
+	if (kill(pid, SIGUSR1) == -1)
+		ft_putstr_fd("3\n", 1);
 }
 
 int	main	(void)
 {
-	/*struct sigaction sa = { 0 };
+	struct sigaction	sa;
+	sigset_t			block_mask;
 
-	sa.sa_handler = &ft_handler;
-	sigaction(SIGUSR1, &sa, NULL);*/
+	sigemptyset(&block_mask);
+	sigaddset(&block_mask, SIGINT);
+	sigaddset(&block_mask, SIGQUIT);
+	sa.sa_handler = 0;
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_mask = block_mask;
+	sa.sa_sigaction = &ft_hdl;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("\n", 1);
+	while (1)
+		pause();
 	return(0);
 }
