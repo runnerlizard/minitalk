5ab1edb122fd08e8b784048df123f0da317c2a5d
Camila Luco <cluco@oa-m1.msk.21-school.ru>
2021-10-08 03:55:43 +0300

    getting first symbol, need to fix others

--- client.c
+++ client.c
@@ -1,78 +1,87 @@
 #include "ft_putnbr_fd.c"
 #include "ft_putstr_fd.c"
-#include <time.h>
 
 int	ft_postman(int serverPid, char *s)
 {
 	static char	*str;
-	//int			i;
+	static int	i;
 
+	if (serverPid == -10)
+	{
+		free(str);
+		exit (0);
+	}
 	if (s != NULL)
 		str = s;
-	ft_putnbr_fd(serverPid, 1);
-	ft_putstr_fd(str, 1);
-	/*if (str[i] == '0')
+	if (str[i] == '0')
 		kill(serverPid, SIGUSR1);
 	else if (str[i] == '1')
 		kill(serverPid, SIGUSR2);
-	i++;*/
-	return(0);
+	i++;
+	return(serverPid);
 }
-/*
-int	ft_hdl_cl(int sig, siginfo_t *info, void *context)
+
+void	ft_hdl_cl(int sig, siginfo_t *info, void *context)
 {
 	(void)context;
+	sleep(1);
 	if (sig == SIGUSR1)
 		ft_postman(info->si_pid, NULL);
 	else
-		exit (0);
-	return (0);
+		ft_postman(-10, NULL);
 }
-*/
+
 char	*ft_transform(char *s, int length)
 {
 	int		n[5];
-	char	utf[length * 26];
+	char	*utf;
+	char	temp[32];
 
 	n[0] = 0;
-	n[1] = 0;
+	n[4] = 0;
+	utf = (char *)malloc(length * 26 + 1);
 	while (s[n[0]])
 	{
-		n[3] = n[1];
-		while (s[n[0]] > 0)
+		n[1] = 0;
+		while(s[n[0]] > 0)
 		{
-			utf[n[1]++ + 5] = s[n[0]] % 2 + 48;
+			temp[n[1]] = s[n[0]] % 2 + 48;
 			s[n[0]] = s[n[0]] / 2;
+			n[1]++;
 		}
-		n[2] = 5;
-		n[4] = n[1];
-		while (n[2]-- > 0)
+		n[2] = n[1];
+		n[3] = 5;
+		while (n[3]-- > 0)
 		{
-			utf[n[3]++] = n[4] % 2 + 48;
-			n[4] = n[4] / 2;
+			temp[n[1]++] = n[2] % 2 + 48;
+			n[2] = n[2] / 2;
 		}
+		while (n[1]-- > 0)		
+			utf[n[4]++] = temp[n[1]];
 		n[0]++;
 	}
-	utf[n[3]] = 0;
+	utf[n[4]] = 0;
 	return (utf);
 }
 
 int	main(int argc, char **argv)
 {
-	/*struct sigaction	sa;
+	struct sigaction	sa_cl;
 	sigset_t			block_mask;
 
-	if (argc != 3)
-		ft_putstr_fd("Error: 2 args required", 1);
 	sigemptyset(&block_mask);
 	sigaddset(&block_mask, SIGINT);
 	sigaddset(&block_mask, SIGQUIT);
-	sa.sa_handler = 0;
-	sa.sa_flags = SA_SIGINFO;
-	sa.sa_mask = block_mask;
-	sa.sa_sigaction = &ft_hdl_cl;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);*/
+	sa_cl.sa_handler = 0;
+	sa_cl.sa_flags = SA_SIGINFO;
+	sa_cl.sa_mask = block_mask;
+	sa_cl.sa_sigaction = &ft_hdl_cl;
+	sigaction(SIGUSR1, &sa_cl, NULL);
+	sigaction(SIGUSR2, &sa_cl, NULL);
+	if (argc != 3)
+		ft_putstr_fd("Error: 2 args required", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("\n", 1);
 	ft_postman(ft_atoi(argv[1]), ft_transform((argv[2]), ft_strlen(argv[2])));
 	while (1)
 		pause();
--- server.c
+++ server.c
@@ -4,52 +4,85 @@
 
 int	ft_sigusr(int ret, int count, int usr, int ver)
 {
-	int		i;
-	char	c;
+	static int		i;
 
-	i = 1;
-	while (--count > 0)
-		i = i * 2;
-	ret = ret + i * usr;
-	if ((n[2] == 0) && (ver == 2))
+	if (ver == -10)
 	{
-		c = ret;
-		write(1, &c, 1);
+		if (count == 0)
+			i = 16;
+		ret = ret + i * usr;
+		i = i / 2;
 	}
-	return (k);
+	else
+	{
+		if (ret == 0)
+		{
+			if (count > 1)
+				i = 1;
+			while (count-- > 1)
+				i = i * 2;
+		}
+		ret = ret + i * usr;
+		i = i / 2;
+	}
+	return (ret);
 }
 
 void	ft_hdl_ser(int sig, siginfo_t *info, void *context)
 {
 	static int	n[4];
+	char		c;
 
 	(void)context;
 	if (n[0] == 0)
 		n[0] = info->si_pid;
 	if (n[1] < 5)
 	{
+		ft_putstr_fd("\nhere\n", 1);
+		ft_putnbr_fd(n[0], 1);
+		ft_putnbr_fd(n[1], 1);
+		ft_putnbr_fd(n[2], 1);
+		ft_putnbr_fd(n[3], 1);
 		if (sig == SIGUSR1)
-			if (kill(n[0], SIGUSR1) != -1)
-				n[2] = ft_sigusr(n[2], n[1]++, 0, 1);
-		else if (sig == SIGUSR2)
-			if (kill(n[0], SIGUSR1) != -1)
-				n[2] = ft_sigusr(n[2], n[1]++, 1, 1);
-	}
-	else 
-	{
-		if (n[2]-- > 1)
-			if (sig == SIGUSR1)
-				if (kill(n[0], SIGUSR1) != -1)
-					n[3] = ft_sigusr(n[3], n[2], 0, 2);
-			else if (sig == SIGUSR2)
-				if (kill(n[0], SIGUSR1) != -1)
-					n[3] = ft_sigusr(n[3], n[2], 1, 2);
-		else
 		{
-			n[0] = 0;
-			n[1] = 0;
-			n[3] = 0;
+			if (kill(n[0], SIGUSR1) != -1)
+				n[2] = ft_sigusr(n[2], n[1]++, 0, -10);
 		}
+		else if (sig == SIGUSR2)
+		{
+			if (kill(n[0], SIGUSR1) != -1)
+				n[2] = ft_sigusr(n[2], n[1]++, 1, -10);
+		}
+	}
+	else if (n[2] > 0)
+	{
+		if (sig == SIGUSR1)
+		{
+			if (kill(n[0], SIGUSR1) != -1)
+				n[3] = ft_sigusr(n[3], n[2], 0, n[2]);
+			ft_putnbr_fd(n[3], 1);
+			ft_putstr_fd("\nsigusr1\n", 1);
+			ft_putnbr_fd(n[2], 1);
+			ft_putstr_fd("\nn2\n", 1);
+		}
+		else if (sig == SIGUSR2)
+		{
+			if (kill(n[0], SIGUSR1) != -1)
+				n[3] = ft_sigusr(n[3], n[2], 1, n[2]);
+			ft_putnbr_fd(n[3], 1);
+			ft_putstr_fd("\nsigusr2\n", 1);
+			ft_putnbr_fd(n[2], 1);
+			ft_putstr_fd("\nn2\n", 1);
+		}
+		n[2]--;
+	}
+	else if (kill(n[0], SIGUSR1) != -1)
+	{
+		c = n[3];
+		write(1, &c, 1);
+		n[0] = 0;
+		n[1] = 0;
+		n[3] = 0;
 	}
 }
 
